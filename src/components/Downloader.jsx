2c86f41ec646edf13b589f595e612b775ea7592e
Boopathiraja S <br2704581@gmail.com>
2025-01-06 17:50:55 +0530

    commands or provided

--- src/components/Downloader.jsx
+++ src/components/Downloader.jsx
@@ -2,16 +2,20 @@ import React, { useState } from 'react'
 
 const Downloader = () => {
 
-    const [songUrl, setSongUrl] = useState("")
-    const [link, setLink] = useState("")
-    const [author, setAuthor] = useState("")
-    const [songName, setSongName] = useState("")
-    const [error, setError] = useState("")
-    const [loading, setLoading] = useState(false)
+    const [songUrl, setSongUrl] = useState("")  // used to update or give songUrl to the url... 
+    const [link, setLink] = useState("")       // to get or extract the download link from the provided url and update the download link
+    const [author, setAuthor] = useState("")  // to get or extract the author name from the provided url and update the author name i mean from "" this to "aniruth"
+    const [songName, setSongName] = useState("") //as follows to update the songname
+    const [error, setError] = useState("")   // if we face any error it update the error and shows to the user
+    const [loading, setLoading] = useState(false)  // this is loading state to show the user the deatails while loading time show the this message[Loading...]
 
     const SongDownload = async () => {
 
         const isValidUrl = (input) => {
+            // to check the provided url is a (url) or not
+            // it check the url is a url like http//.... .com
+            // it is a function that we can use this isValidUrl to any input fields. In here we have only one input field contain url state. 
+            // so that's why we use this to prevent future error that may come by users
             try {
                 new URL(input)
                 return true
@@ -22,11 +26,16 @@ const Downloader = () => {
         }
 
         if (!songUrl.trim() || !isValidUrl(songUrl)) {
+
             setError("Provide valid url to get Song Deatails...")
-            setSongUrl("")
+            // to check the provided url is a (valid url) or not
+            // it check the url is wrong or right i mean url is work or not or spelling mistake's
+
+            setSongUrl("") // if its not valid it clear the input field so thats we we update the state ("")
             return
         }
 
+        // rapid api
         const url = `https://spotify-downloader9.p.rapidapi.com/downloadSong?songId=${songUrl}`;
         const options = {
             method: 'GET',
@@ -36,11 +45,11 @@ const Downloader = () => {
             }
         };
 
-        setLoading(true)
-        setLink("")
-        setAuthor("")
-        setSongName("")
-        setError("")
+        setLoading(true)  // while fetching the data, the load state update to true
+        setLink("")  // while fetching the data, the link must be empty
+        setAuthor("")  // as follows the setLink
+        setSongName("") // as follows the setLink
+        setError("")  // as follows the setLink because on the proccess there is no error after completion if we face it update by upcomming states
 
         try {
             const response = await fetch(url, options);
@@ -51,31 +60,37 @@ const Downloader = () => {
             console.log(result.data);
 
             if (result.data) {
+                // if data is there it follow the if block. and update the data to every state that respected filed state
+                // like link contain it download link data
+                // author contain its author name data
+                // song contain songName
                 setLink(downlodlink)
                 setAuthor(songAuthor)
                 setSongName(songname)
             }
             else {
                 setError("There must be an error in link...")
+                // if the url is wrong it set the error as contain the above
             }
 
         } catch (error) {
             console.error(error);
             setError("There must be an error try again later...")
+            // if we can't fetch the data it gives error as mention above
         }
         finally {
-            setLoading(false)
-            setSongUrl("")
+            setLoading(false)  // after getting data successfully it update the loading state to false
+            setSongUrl("")  // and clear the url
         }
     }
     function handleMusicLink(e) {
-        setSongUrl(e.target.value)
+        setSongUrl(e.target.value)  // onChange event to update songUrl
     }
 
     return (
         <div className=''>
             <div className='my-32 flex flex-col gap-10 justify-center items-center'>
-                {/* url */}
+                {/* url contain input and button tags */}
                 <div className='flex flex-col md:flex-row gap-4 items-center'>
                     <input
                         type="url"
@@ -85,7 +100,7 @@ const Downloader = () => {
                         placeholder='Give url to get song...' />
                     <button
                         onClick={SongDownload}
-                        className= {`rounded-lg h-8 md:h-10 text-lg font-semibold text-white transition-all px-2 md:w-32 ${loading || !songUrl.trim() ? "cursor-not-allowed bg-slate-700":"bg-green-500 hover:bg-green-600 hover:scale-105" }`}
+                        className={`rounded-lg h-8 md:h-10 text-lg font-semibold text-white transition-all px-2 md:w-32 ${loading || !songUrl.trim() ? "cursor-not-allowed bg-slate-700" : "bg-green-500 hover:bg-green-600 hover:scale-105"}`}
                         disabled={loading || !songUrl.trim()}
                     >
                         GetLink
@@ -100,13 +115,16 @@ const Downloader = () => {
                     {
                         !loading && !error && !link && !songName && !author && (
                             <p className='text-xl font-xl text-green-600 text-center'>Give url to download your Songs</p>
+                            // if there is no data or the first entering stage
                         )
                     }
                     {
                         loading ? (
                             <p className='text-xl font-xl text-green-600 text-center'>Loading...</p>
+                            // if loading time is true it show the Loading... messsage 
                         ) :
                             (
+                                // if there is Download link, author, songName available it give the data
                                 link ? (
                                     <div className='flex flex-col gap-2'>
                                         <p className='text-green-700 font-semibold'>Music name :
@@ -126,6 +144,7 @@ const Downloader = () => {
                                 )
                                     :
                                     (
+                                        // if there is any error or url wrong or our side error it shows that message
                                         <p className='text-xl font-xl text-red-600 text-center'>{error}</p>
                                     )
                             )
